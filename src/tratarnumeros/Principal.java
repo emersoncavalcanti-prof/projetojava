81e5363b2bad33f8de0815e326b63740545daaf0
Emerson Cavalcanti <emerson.cavalcanti@escolatecnicalimoeiro.com.br>
2024-06-13 15:04:27 -0300

    add do while para controlar o programa

--- src/tratarnumeros/Principal.java
+++ src/tratarnumeros/Principal.java
@@ -9,21 +9,42 @@ public class Principal {
     static Scanner leia = new Scanner(System.in);
 
     public static void main(String[] args) {
-
-        System.out.println("#### MEU SOFTWARE ####");
-        System.out.println("1-Descobrir se o numero e par ou impar");
-        System.out.println("2-Descobrir se o numero e positivo ou negativo");
-        System.out.print("Escolha uma opcao ou digite qualquer tecla para sair: ");
-
-        opcao = leia.next();
-
-        if (opcao.equals("1")) {
-            parOuImpar();
-        } else if (opcao.equals("2")) {
-            positivoOuNegativo();
-        } else {
-            System.out.println("Sair do programa");
+        
+        //COMENTARIOS PARA ESTUDO	
+        /*for(int indice=10;indice <=  5;indice++){
+            System.out.println("Valor de indice do for: "+indice);
         }
+        
+        int indice = 10; 
+        while(indice <= 5){
+           System.out.println("Valor de indice do while: "+indice);  
+           indice++;
+        }
+        indice = 10;
+        do{
+          System.out.println("Valor de indice com o do while: "+indice);  
+          indice++;   
+        }while(indice <= 5); */
+		
+        do{
+            System.out.println("#### MEU SOFTWARE ####");
+            System.out.println("1-Descobrir se o numero e par ou impar");
+            System.out.println("2-Descobrir se o numero e positivo ou negativo");
+            System.out.print("Escolha uma opcao");
+
+            opcao = leia.next();
+
+            if (opcao.equals("1")) {
+                parOuImpar();
+            } else if (opcao.equals("2")) {
+                positivoOuNegativo();
+            } else{
+                System.out.println("Opção nao encontrada");
+            }
+            
+            System.out.println("Digite sair para finalizar ou qualquer outra tecla para continuar.");
+            opcao = leia.next();
+        }while(!opcao.equals("sair"));
     }
 
     public static void parOuImpar() {
